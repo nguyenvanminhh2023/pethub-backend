561659959412f6319ae608950de2c08bc337b0d2
nguyenvanminhh2023 <minhyp01@gmail.com>
2023-04-25 17:56:48 +0700

    ...

--- models/user.model.js
+++ models/user.model.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const Schema = mongoose.Schema;
-
 const userSchema = new Schema({
     email: { type: String, require: true, unique: true },
     password: { type: String, required: true },
